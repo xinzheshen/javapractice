93604ebdd4b76ebdeec47621fcf496a1cbca2584
shenxinzhe <xinzhe.shen@cienet.com.cn>
2019-02-21 11:03:51 +0800

    add javautil observer

--- src/main/java/com/sxz/study/Test.java
+++ src/main/java/com/sxz/study/Test.java
@@ -1,10 +1,30 @@
 package com.sxz.study;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Test {
 
     public static void main(String[] args) {
-        String str = "womenbuyiyang";
-        String[] split = str.split(".");
-        System.out.println(split[0]);
+//        String str = "womenbuyiyang";
+//        String[] split = str.split(".");
+//        System.out.println(split[0]);
+
+        List<String> list1 =new ArrayList();
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+
+        StringBuilder sb = new StringBuilder();
+        String result = list1.stream().reduce("", String::concat);
+        System.out.println(result);
+
+        list1.forEach(string -> sb.append(string));
+        System.out.println(sb.toString());
+
+        String join = String.join(",", list1);
+        System.out.println(join);
+
     }
 }
---
+++ src/main/java/com/sxz/study/model/javautil/Observer1.java
@@ -0,0 +1,35 @@
+package com.sxz.study.model.javautil;
+
+import org.apache.log4j.Logger;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * 最后是我们的使用者：
+ */
+
+public class Observer1 implements Observer {
+    private static Logger log = Logger.getLogger(Observer1.class);
+
+    public void registerSubject(Observable observable) {
+        observable.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o instanceof SubjectFor3d) {
+            SubjectFor3d subjectFor3d = (SubjectFor3d) o;
+            log.info("SubjectFor3d:subjectFor3d's msg -- >" + subjectFor3d.getMsg());
+
+//            Toast.makeText(PatternApplication.getInstance(), "subjectFor3d's msg -- >" + subjectFor3d.getMsg(), Toast.LENGTH_SHORT).show();
+        }
+        if (o instanceof SubjectForSSQ) {
+            SubjectForSSQ subjectForSSQ = (SubjectForSSQ) o;
+            log.info("SubjectForSSQ:subjectForSSQ's msg -- >" + subjectForSSQ.getMsg());
+
+//            Toast.makeText(PatternApplication.getInstance(), "subjectForSSQ's msg -- >" + subjectForSSQ.getMsg(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
