d0b8ccf8fafbcff23b24897fec20f24435506d6d
shenxinzhe <xinzhe.shen@cienet.com.cn>
2019-02-27 17:18:06 +0800

    update

--- src/main/java/com/sxz/study/Test.java
+++ src/main/java/com/sxz/study/Test.java
@@ -11,20 +11,51 @@ public class Test {
 //        String[] split = str.split(".");
 //        System.out.println(split[0]);
 
-        List<String> list1 =new ArrayList();
-        list1.add("1");
-        list1.add("2");
-        list1.add("3");
+//        List<String> list1 =new ArrayList();
+//        list1.add("1");
+//        list1.add("2");
+//        list1.add("3");
+//
+//        StringBuilder sb = new StringBuilder();
+//        String result = list1.stream().reduce("", String::concat);
+//        System.out.println(result);
+//
+//        list1.forEach(string -> sb.append(string));
+//        System.out.println(sb.toString());
+//
+//        String join = String.join(",", list1);
+//        System.out.println(join);
 
-        StringBuilder sb = new StringBuilder();
-        String result = list1.stream().reduce("", String::concat);
-        System.out.println(result);
 
-        list1.forEach(string -> sb.append(string));
-        System.out.println(sb.toString());
+        String data = "test**********";
+        Boolean isCyclic = false;
 
-        String join = String.join(",", list1);
-        System.out.println(join);
+        System.out.println(String.format("data=%s&isCyclic=%s", data, isCyclic));
+        System.out.println(String.format("data=%d&isCyclic=%s", (byte)128, isCyclic));
 
+
+        byte b1 = (byte)127;
+        byte b2 = (byte)128;
+
+        System.out.println("int 127: " + b1);
+        System.out.println("int 128: " + b2);
+        System.out.println("int 128: " + (b2 & 0xFF));
+
+        new Test().testShort((short)4444);
+
+        System.out.println("------------------");
+
+        Integer i = 11;
+        System.out.println("Integer 11 to string: " + i.toString());
+
+        Integer num1 = 288;
+        int num2 = 288;
+        System.out.println("Integer 288 to string: " + (byte)num2);
+        System.out.println("Integer 288 to string: " + ((byte)num2 & 0xFF));
+
+    }
+
+    void testShort(short num){
+        System.out.println(num);
     }
 }
